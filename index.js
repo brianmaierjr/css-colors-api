827f040ed67a189efb5be4a6db5e02ab2ce8b5de
Brian Maier <brimaidesigns@gmail.com>
2023-03-16 19:46:05 -0400

    + full json

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const app = express();
+let colorRepo = require("./repos/colorRepo");
 
-const data = [
-	{
-		name: "blue",
-		hex: "#0000FF",
-	},
-	{
-		name: "red",
-		hex: "#FF0000",
-	},
-];
+// use cors to allow cross origin resource sharing
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api", (req, res) => {
-	res.send(data);
+app.get("/api/colors", (req, res, next) => {
+	colorRepo.get(
+		function (data) {
+			res.status(200).json({
+				status: 200,
+				statusText: "OK",
+				message: "All css colors retrieved.",
+				data: data,
+			});
+		},
+		function (err) {
+			next(err);
+		}
+	);
 });
+
+// /colors/ gets all colors
+// /colors/{name} gets a color by name
+// /colors/groups gets all color groups
+// /colors/groups/{name} gets all colors in a group
 
 app.listen(4000, () => {
 	console.log("Running on port 4000.");
